148dcd156cdebbbe99941091211e593950c3e508
Kaushal Soni <kaushal.soni@searce.com>
2022-05-18 15:00:51 +0530

    updated to first version and added tag

--- http-server.go
+++ http-server.go
@@ -1,12 +1,13 @@
 package main
-import(
+
+import (
 	//"fmt"
 	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hello World from GO \n"))
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World from GO\n Deploying Server version 1.0 \n"))
 	})
-    http.ListenAndServe(":80",nil)
+	http.ListenAndServe(":80", nil)
 }
