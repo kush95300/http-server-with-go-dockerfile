1c39666efaac72661880f583a9bf708e2f43fca8
Kaushal Soni <kaushal.soni@searce.com>
2022-05-18 18:34:00 +0530

    updated to v3

--- http-server.go
+++ http-server.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello World from GO HTTP Server </h1>\n\n <b>Deploying Server version v2 on Cloud RUN </b>\n"))
+		w.Write([]byte("<h1>Hello World from GO HTTP Server </h1>\n\n <h2>Deploying Server version v3 on Cloud RUN </h2>\n"))
 	})
 	http.ListenAndServe(":80", nil)
 }
