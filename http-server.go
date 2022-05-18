1571f4a8c23085689dd68a612e718a5131da8b8a
Kaushal Soni <kaushal.soni@searce.com>
2022-05-18 15:19:17 +0530

    created first version of app

--- http-server.go
+++ http-server.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World from GO\n Deploying Server version 1.0 \n"))
+		w.Write([]byte("<h1>Hello World from GO HTTP Server </h1>\n\n <b>Deploying Server version v1 </b>\n"))
 	})
 	http.ListenAndServe(":80", nil)
 }
